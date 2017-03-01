67b3624dfc28859907d72af1289ba7cdd3796cbc
Drew Jaynes <drew@affiliatewp.com>
2017-03-01 15:18:05 -0700

    Docs for the AffWP\Labs\Feature class.

--- includes/abstracts/class-affwp-labs-feature.php
+++ includes/abstracts/class-affwp-labs-feature.php
@@ -1,8 +1,19 @@
 <?php
 namespace AffWP\Labs;
 
+/**
+ * Abstract base class for defining a Labs feature.
+ *
+ * @since 2.0.4
+ */
 abstract class Feature {
 
+	/**
+	 * Runs during instantiation of a single Labs feature.
+	 *
+	 * @access public
+	 * @since  2.0.4
+	 */
 	public function __construct() {
 		add_filter( 'affwp_settings_labs', array( $this, 'register_labs_setting' ) );
 	}
