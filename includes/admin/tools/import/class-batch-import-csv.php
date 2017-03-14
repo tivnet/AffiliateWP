a9eb5163aabcb608a8ef90f5c6ca03bb045c6c04
Drew Jaynes <drew@affiliatewp.com>
2017-03-14 16:14:48 -0600

    Ensure \AffWP\Utils\Batch_Process\Import\CSV defines all required methods.

--- includes/admin/tools/import/class-batch-import-csv.php
+++ includes/admin/tools/import/class-batch-import-csv.php
@@ -86,4 +86,66 @@ class CSV extends Batch\Import implements Importer\CSV {
 		$this->field_mapping = $import_fields;
 	}
 
+	/**
+	 * Prepares the data for import.
+	 *
+	 * @access public
+	 * @since  2.0
+	 *
+	 * @return array[] Multi-dimensional array of data for import.
+	 */
+	public function get_data() {}
+
+	/**
+	 * Performs the import process.
+	 *
+	 * @access public
+	 * @since  2.0
+	 *
+	 * @return void
+	 */
+	public function import() {}
+
+	/**
+	 * Sets the CSV columns.
+	 *
+	 * @access public
+	 * @since  2.1
+	 *
+	 * @return array<string,string> CSV columns.
+	 */
+	public function csv_cols() {}
+
+	/**
+	 * Retrieves the CSV columns array.
+	 *
+	 * Alias for csv_cols(), usually used to implement a filter on the return.
+	 *
+	 * @access public
+	 * @since  2.1
+	 *
+	 * @return array<string,string> CSV columns.
+	 */
+	public function get_csv_cols() {}
+
+	/**
+	 * Outputs the CSV columns.
+	 *
+	 * @access public
+	 * @since  2.1
+	 *
+	 * @return void
+	 */
+	public function csv_cols_out() {}
+
+	/**
+	 * Outputs the CSV rows.
+	 *
+	 * @access public
+	 * @since  2.1
+	 *
+	 * @return void
+	 */
+	public function csv_rows_out() {}
+
 }
