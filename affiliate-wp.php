8db8792955bf660a6e23b0a52193378debec370f
Drew Jaynes <DrewAPicture@users.noreply.github.com>
2017-03-15 17:27:45 -0600

    issue/2104 – Abstract out the Registry class for reuse elsewhere in core (#2107) Fixes #2104
    * Abstract out the Registry class for reuse elsewhere in core.
    
    * Try just passing the class name as a string for Travis' benefit.
    
    * Docs updates.

--- affiliate-wp.php
+++ affiliate-wp.php
@@ -346,6 +346,7 @@ final class Affiliate_WP {
 		require_once AFFILIATEWP_PLUGIN_DIR . 'includes/class-affwp-visit.php';
 
 		require_once AFFILIATEWP_PLUGIN_DIR . 'includes/actions.php';
+		require_once AFFILIATEWP_PLUGIN_DIR . 'includes/abstracts/class-affwp-registry.php';
 		require_once AFFILIATEWP_PLUGIN_DIR . 'includes/admin/settings/class-settings.php';
 		require_once AFFILIATEWP_PLUGIN_DIR . 'includes/abstracts/class-db.php';
 		require_once AFFILIATEWP_PLUGIN_DIR . 'includes/class-affiliates-db.php';
